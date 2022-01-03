693d15ad16515f15521ed3a9fe43d540a7b52497
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-03 17:50:23 +0100

    Using sha256 to hash user password

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import os
 
 def generateSecretKey():
@@ -74,7 +76,8 @@ def signup():
 		form = RegisterForm()
 
 		if form.validate_on_submit():
-				new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+				hashed_password = generate_password_hash(form.password.data, method='sha256')
+				new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
 				db.session.add(new_user)
 				db.session.commit()
 				return redirect(url_for('login'))
@@ -88,7 +91,7 @@ def login():
 		if form.validate_on_submit():
 				user = User.query.filter_by(username=form.username.data).first()
 				if user:
-						if (user.password == form.password.data):
+						if check_password_hash(user.password, form.password.data):
 								login_user(user)
 								return redirect(url_for('index'))
 
--- db_operations.py
+++ db_operations.py
@@ -1,7 +1,7 @@
 from app import db
 from app import Note 
 
-new = Note(content="Another note", user_id=1, isEncrypted=False)
+new = Note(content="Secret note :)", user_id=2, isEncrypted=False)
 
 db.session.add(new)
 db.session.commit()
