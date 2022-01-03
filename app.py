8a01cb992e65a44315a6575ca31a975498fa8eae
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-03 15:46:24 +0100

    Add User (with id, username, email, password) table to database

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from wtforms.validators import InputRequired, Email, Length
 
 from flask_bootstrap import Bootstrap
 
+from flask_sqlalchemy import SQLAlchemy
+
 import os
 
 def generateSecretKey():
@@ -15,7 +17,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = generateSecretKey()
 
+db = SQLAlchemy(app)
+
 Bootstrap(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
@@ -31,6 +41,9 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
         return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
