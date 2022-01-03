3eedbad9b868e5c0b6c65127e40ca0a8bbf66b71
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-03 17:40:58 +0100

    Encrypting and Decrypting notes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 import os
 
 def generateSecretKey():
-  return os.urandom(32)
+	return os.urandom(32)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -29,83 +29,100 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+		return User.query.get(int(user_id))
 
 @app.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    return redirect(url_for('index'))
+		logout_user()
+		return redirect(url_for('index'))
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
-    notes = db.relationship('Note', backref='user', lazy='dynamic')
+		id = db.Column(db.Integer, primary_key=True)
+		username = db.Column(db.String(15), unique=True)
+		email = db.Column(db.String(50), unique=True)
+		password = db.Column(db.String(80))
+		notes = db.relationship('Note', backref='user', lazy='dynamic')
 
 class Note(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	content = db.Column(db.String())
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	password = db.Column(db.String())
+	isEncrypted = db.Column(db.Boolean())
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+		email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+		username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+		password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 class LoginForm(FlaskForm):
-  username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-  password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 class EncryptForm(FlaskForm):
-  password = PasswordField('password')
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
 @login_required
 def index():
-    notes = Note.query.filter(Note.id == current_user.id)
-    return render_template('index.html', notes=notes, name=current_user.username)
+		notes = Note.query.filter(Note.user_id == current_user.id)
+		return render_template('index.html', notes=notes, name=current_user.username)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = RegisterForm()
+		form = RegisterForm()
 
-    if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
+		if form.validate_on_submit():
+				new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+				db.session.add(new_user)
+				db.session.commit()
+				return redirect(url_for('login'))
 
-    return render_template('signup.html', form=form)
+		return render_template('signup.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+		form = LoginForm()
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if (user.password == form.password.data):
-                login_user(user)
-                return redirect(url_for('index'))
+		if form.validate_on_submit():
+				user = User.query.filter_by(username=form.username.data).first()
+				if user:
+						if (user.password == form.password.data):
+								login_user(user)
+								return redirect(url_for('index'))
 
-        return '<h1>Invalid username or password</h1>'
-        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+				return '<h1>Invalid username or password</h1>'
+				# return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
-    return render_template('login.html', form=form)
+		return render_template('login.html', form=form)
 
 @app.route('/encrypt/<id>', methods=['GET', 'POST'])
+@login_required
 def encrypt(id):
-    form = EncryptForm()
-    note = Note.query.filter_by(id=id).first()
-    if form.validate_on_submit():
-      note.password = form.password.data
-      db.session.commit()
-      return redirect(url_for('index'))
-    return render_template('encrypt.html', form=form, note=note)
+		form = EncryptForm()
+		note = Note.query.filter_by(id=id).first()
+		if form.validate_on_submit():
+			note.isEncrypted = 1
+			note.password = form.password.data
+			db.session.commit()
+			return redirect(url_for('index'))
+		return render_template('encrypt.html', form=form, note=note)
 
+@app.route('/decrypt/<id>', methods=['GET', 'POST'])
+@login_required
+def decrypt(id):
+	form = EncryptForm()
+	note = Note.query.filter_by(id=id).first()
+
+	if form.validate_on_submit():
+		if (note.password == form.password.data):
+			note.password = ''
+			note.isEncrypted = 0
+			db.session.commit()
+			return redirect(url_for('index'))
+
+	return render_template('decrypt.html', form=form, note=note)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+	app.run(debug=True)
--- db_operations.py
+++ db_operations.py
@@ -1,7 +1,7 @@
 from app import db
 from app import Note 
 
-new = Note(content="Another note", user_id=1)
+new = Note(content="Another note", user_id=1, isEncrypted=False)
 
 db.session.add(new)
 db.session.commit()
