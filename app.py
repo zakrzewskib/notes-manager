3a09c3a8be92497d5617e30ea6fa5328fdfe6139
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-03 16:32:08 +0100

    Show notes that belong to logged user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
@@ -7,6 +7,8 @@ from wtforms.validators import InputRequired, Email, Length
 from flask_bootstrap import Bootstrap
 
 from flask_sqlalchemy import SQLAlchemy
+
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 import os
 
@@ -21,7 +23,15 @@ db = SQLAlchemy(app)
 
 Bootstrap(app)
 
-class User(db.Model):
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
@@ -38,10 +48,14 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
+class LoginForm(FlaskForm):
+  username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+  password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
 @app.route('/')
+@login_required
 def index():
-    notes = Note.query.filter(Note.id == 2)
-    # notes = Note.query.filter(Note.id == 1)
+    notes = Note.query.filter(Note.id == current_user.id)
     return render_template('index.html', notes=notes)
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -55,6 +69,22 @@ def signup():
         return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if (user.password == form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+
+        return '<h1>Invalid username or password</h1>'
+        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
 
 if __name__ == "__main__":
   app.run(debug=True)
