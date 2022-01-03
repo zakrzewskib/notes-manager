623b6790708d088b58b695a5ad6e34936f76006a
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-03 13:27:26 +0100

    Add Signup page from
    https://www.youtube.com/watch?v=8aTnmsDMldY

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 from flask import Flask, render_template
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+from flask_bootstrap import Bootstrap
+
+import os
+
+def generateSecretKey():
+  return os.urandom(32)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = generateSecretKey()
+
+Bootstrap(app)
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
   app.run(debug=True)
