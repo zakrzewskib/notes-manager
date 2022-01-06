4c9194adcdf52a3db09f8c9869e76de3d979c8c9
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-06 18:02:29 +0100

    Add comment about symmetric encryption

--- utilities/encryption.py
+++ utilities/encryption.py
@@ -1,3 +1,8 @@
+# I want to use symmetric encryption - use the same password/key for encryption and decryption
+
+# I used ARC4 as first algorithm to encrypt notes, but it is suggested to use more secure AES
+# However in AES key need to be size of 16 bytes, and for now I cannot make it work
+
 from Crypto.Cipher import ARC4
 
 def encryptMessage(message, password):
@@ -10,3 +15,4 @@ def decryptMessage(encryptedMessage, password):
   decrypted = cipher.decrypt(encryptedMessage)
   return decrypted.decode()
 
+print(decryptMessage(encryptMessage('secret message', 'password'), 'password'))
