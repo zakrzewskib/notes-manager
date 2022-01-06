f71fd5b7132a2bc08d2b11a1039ab7248e8c7b9d
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-06 18:23:06 +0100

    Add comment about hashing

--- utilities/hashing.py
+++ utilities/hashing.py
@@ -1,5 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# https://techmonger.github.io/4/secure-passwords-werkzeug/
+# generate_password_hash by default produces salt string with length 8 (which means we hash(password + salt))
+
 def hashPassword(password):
   return generate_password_hash(password, method='sha256')
 
