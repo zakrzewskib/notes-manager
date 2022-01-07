1ab18ebf8320e41782ab5fcdf13b0ad88d4991c2
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-07 15:57:45 +0100

    Add comment that WTForms should protect against attacks, such as SQL injection

--- utilities/forms.py
+++ utilities/forms.py
@@ -3,6 +3,11 @@ from wtforms import StringField, PasswordField, BooleanField, SelectMultipleFiel
 from wtforms.widgets import TextArea
 from wtforms.validators import InputRequired, Email, Length
 
+# https://subscription.packtpub.com/book/web-development/9781784393656/3/ch03lvl1sec22/flask-wtforms
+# WTForms is a library that handles server form validation for you by checking input against common form types.
+# Flask WTForms is a Flask extension on top of WTForms that add features, such as Jinja HTML rendering, 
+# and protects you against attacks, such as SQL injection and cross-site request forgery.
+
 class RegisterForm(FlaskForm):
 		email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
 		username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
